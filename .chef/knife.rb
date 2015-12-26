abc0a171e4f7e1216c96d461ea821eee255b56b7
Shashi Yebbare <shashi.kshatriyan@gmail.com>
2015-12-26 13:55:51 +0530

    Excluding the .chef folder from Git

---
+++ .chef/knife.rb
@@ -0,0 +1,11 @@
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "shashi_kshatriyan"
+client_key               "#{current_dir}/shashi_kshatriyan.pem"
+validation_client_name   "sky_corporation-validator"
+validation_key           "#{current_dir}/sky_corporation-validator.pem"
+chef_server_url          "https://api.chef.io/organizations/sky_corporation"
+cookbook_path            ["#{current_dir}/../cookbooks"]
